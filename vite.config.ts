c6eabe01a07c8da4ccedf5d8b6c09aa4a1be35b4
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2024-05-27 17:40:46 -0400

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/BaniFrames/", // Set this to your repository name
-  base: "", // Set this to your repository name
+  base: "/BaniFrames/", // Set this to your repository name
+  // base: "", // Set this to your repository name
   build: {
     outDir: "dist",
   },
